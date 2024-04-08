f5cbdfbf8786b7b3ae2cb147cbfcabb36ad87a44
Vinícius Osório <vinicius.osorio@outlook.com>
2024-04-08 14:00:08 -0300

    nova branch, class pacote, coletar pacote

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from math import floor, ceil
+from decimal import Decimal
 
 continuar = True
 menu = 0
-cargaMax = 0
-volumeMax = 0
+cargaMax = 800
+cargaUsada = 0
+volumeMax = 200
+volumeUsado = 0
+pacotes = []
+contPacDia = 0
+
+#Classe de pacote individual
+class Pacote:
+    identificador = 0
+    peso = 0
+    valorUnit = 0
+
+    def __init__(self, identificador, peso, valor) -> None:
+        self.identificador = identificador
+        self.peso = peso
+        self.valorUnit = valor
+
+
 
 #Mensagem de início
 print("Bem-vindo ao sistema de controle de carga!")
@@ -11,8 +29,14 @@ print("Bem-vindo ao sistema de controle de carga!")
 #Laço de repetição do sistema. Ao encerrar o dia, usuário pode escolher reiniciar o sistema.
 while continuar:
     #Menu de ações
-    print("Ações Possíveis:")
-    print("\n1- Iniciar o Dia\n2- Realizar Parada\n3- Consultar Situação\n4- Mostrar Pacotes\n5- Encerrrar o Dia\n6- Gerar Relatório\n7- Encerrar o Sistema")
+    print("Ações Possíveis:\n",
+          "1- Iniciar o Dia\n",
+          "2- Realizar Parada\n",
+          "3- Consultar Situação\n",
+          "4- Mostrar Pacotes\n",
+          "5- Encerrrar o Dia\n",
+          "6- Gerar Relatório\n",
+          "7- Encerrar o Sistema")
     menu = input("Informe sua opção: ")
 
     match menu:
@@ -29,14 +53,14 @@ while continuar:
             print(cargaMax.isdigit())
             if (str(cargaMax.replace(".","1")).isdigit()):
                 cargaMax = float(cargaMax)
-                print(cargaMax, type(cargaMax))
+                # print(cargaMax, type(cargaMax))
 
             while not (str(cargaMax).replace(".", "1").isdigit() or type(cargaMax) != str):
                 print(cargaMax)
                 cargaMax = input("Valor inválido. Informe valor válido para capacidade de carga máxima (Kg):\n")
                 if (str(cargaMax).replace(".","1").isdigit()):
                     cargaMax = float(cargaMax)
-                    print(cargaMax, type(cargaMax))
+                    # print(cargaMax, type(cargaMax))
             
 
             #Entrada de dado: capacidade de volume máximo
@@ -54,8 +78,48 @@ while continuar:
                     print(volumeMax, type(volumeMax))
             print("\nDados do caminhão registrado!\nPeso máximo:", cargaMax,"kg\nVolume Máximo:", volumeMax,"m³\n\n")
 
-        case "2":
-            print("Em Breve")
+        case "2": #Parada
+            operacao = 0
+
+            while operacao != "c":
+
+                operacao = input("\nInforme a opção de parada:\na- Coletar Pacote\nb- Entregar Pacote\nc- Retomar Viagem\n\n")
+                match operacao: #Opções das ações de paradas
+                    case "a": #Coletar pacote
+                        
+                        pesoEnt = input("\nInforme o peso(Kg) do pacote coletado: ")
+                        if not pesoEnt.replace(".", "1").isdigit(): #Testando se valor é válido
+                            pesoEnt = input("Valor inválido! Favor, informar o peso(Kg) do pacote coletado: ")
+                            print(float(pesoEnt))
+                        if (cargaMax > (cargaUsada + float(pesoEnt))): #Testando se novo pacote exederá o peso máximo
+                            confValor = 0
+                            
+                            print("Valor do transporte: R$",Decimal(float(pesoEnt)*1.5))
+                            confValor = input("Confirma valor? S/N\n")
+                            while (confValor != "S" or confValor != "s" or confValor != "N" or confValor != "n"):
+                                if (confValor == "S" or confValor == "s"):
+                                    cargaUsada += float(pesoEnt)
+                                    pac = Pacote(contPacDia, pesoEnt, Decimal(float(pesoEnt)*1.5))
+                                    pacotes.append(pac)
+                                    contPacDia+=1
+                                    print(cargaUsada, contPacDia, pac.valorUnit)
+                                    break
+                                elif (confValor == "N" or confValor == "n"):
+                                    print("Valor não-aceito. Cancelando coleta.")
+                                    break
+                                else:
+                                    confValor = input("Opção inválida. Confirma valor? S/N")
+
+                        else:
+                            print("\nPeso excederá capacidade do veículo. Cancelando coleta.\n")
+
+                    case "b":
+                        print("Entrega")
+                    case "c":
+                        print("Segue viagem")
+                    case _:
+                        print("Opção Inválida!")
+
 
         case "3":
             print("Em Breve")
